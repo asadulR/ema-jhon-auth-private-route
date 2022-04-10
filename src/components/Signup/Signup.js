e8ed32ff1a5aaec546c41a75627ea66f22885143
asadulR <asad1252001@gmail.com.com>
2022-04-10 17:06:50 +0600

    signup with eemail pass  setup

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+
 import '../Login/Login.css'
+import auth from '../../firebase.init';
 const Signup = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
-
+    const navigate = useNavigate();
 
 
     const handleEmailInput = (event) => {
@@ -20,30 +23,52 @@ const Signup = () => {
         setConfirmPassword(event.target.value);
     }
 
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
 
+    const handleCreateUser = (event) => {
+        event.preventDefault();
+
+        if (password !== confirmPassword) {
+            setError("Password didn.t match!")
+            return;
+        }
+
+        createUserWithEmailAndPassword(email, password)
+        .then(result => {
+            // const user = result.user
+            console.log('user created');
+        })
+
+    }
+
+    
+    if(user) {
+        navigate('/');
+    }
 
     return (
         <div className='login-form'>
-            <form>
+            <form onSubmit={handleCreateUser}>
                 <h2>Sign up</h2>
                 <div className='email-field'>
                     <label className='input-label' htmlFor='email'>Email</label>
                     <div className='mt'>
-                        <input onBlur={handleEmailInput} type="email" name="email" id="email" placeholder='Enter your email' required/>
+                        <input onBlur={handleEmailInput} type="email" name="email" id="email" placeholder='Enter your email' required />
                     </div>
                 </div>
                 <div className='password-field'>
                     <label className='input-label' htmlFor='password'>Password</label>
                     <div className='mt'>
-                        <input onBlur={handlePasswordInput} type="password" name="password" id="password" placeholder='Your password' required/>
+                        <input onBlur={handlePasswordInput} type="password" name="password" id="password" placeholder='Your password' required />
                     </div>
                 </div>
                 <div className='password-field'>
                     <label className='input-label' htmlFor='confirmPassword'>Confirm password</label>
                     <div className='mt'>
-                        <input onBlur={handleConfirmPasswordInput} type="password" name="confirmPassword" id="confirmPassword" placeholder='Confirm your password' required/>
+                        <input onBlur={handleConfirmPasswordInput} type="password" name="confirmPassword" id="confirmPassword" placeholder='Confirm your password' required />
                     </div>
                 </div>
+                <p className='error'>{error}</p>
                 <div className='submit-btn-container'>
                     <button type="submit">Signup</button>
                 </div>
